261c39338733b9564b7dd1fc20a313987e2371fd
xvzc <dev.kwanghoo@gmail.com>
2023-01-23 14:31:07 +0900

    re-init

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
-import setuptools
-setuptools.setup(
+from setuptools import setup
+setup(
+    entry_points = {
+        'console_scripts': ['hello=mymodule.command_line:cli'],
+    },
     name='hello-world',
-    version='1.0',
-    scripts=['./scripts/hello-runner'],
-    author='xvzc',
-    description='This runs my script which is great.',
-    packages=['lib.hello'],
-    install_requires=[
-        'setuptools',
-    ],
-    python_requires='>=3.5'
 )
