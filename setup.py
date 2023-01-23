86d394098500e287648fdebf4f36f31923330018
xvzc <dev.kwanghoo@gmail.com>
2023-01-23 14:33:16 +0900

    awef

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     entry_points = {
-        'console_scripts': ['hello=hello-world.command_line:cli'],
+        'console_scripts': ['hello=hello.command_line:cli'],
     },
     name='hello-world',
 )
